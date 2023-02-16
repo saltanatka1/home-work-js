c00bbcf751e4e1cb52678b6bc4dacfa5e2200a48
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-02-17 05:45:44 +0600

    create a function that prints the volume of a cone

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function printname(firstName , lastName){
   console.log("My name is " + firstName +" " + lastName);
 }
+
+function printVolumeCone(height , radius){
+  let result = (1/3) *3.14 * radius * height
+  console.log("Объем конуса равен " + result );
+}
