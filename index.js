62c84e3729d9707eebef7c4132fca01e556e60e8
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-02-17 06:35:41 +0600

    loop to find the squares of numbers

--- index.js
+++ index.js
@@ -42,3 +42,9 @@ for (let i = 1; i <= 8; i++) {
   total = total * i
 }  
 console.log( "8! = " + total);
+
+square = 1;
+for (let i = 1; i <= 10; i++) {
+  square = i ** 2;
+  console.log( square);
+}  
