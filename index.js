e54b0dba8c9b4e86bcc83d13b2556b2c2e7fae56
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-02-17 14:09:08 +0600

    checks if there is such a word in the array

--- index.js
+++ index.js
@@ -1,103 +1,116 @@
-function printname(firstName , lastName){
-  console.log("My name is " + firstName +" " + lastName);
+function printname(firstName, lastName) {
+  console.log("My name is " + firstName + " " + lastName);
 }
 
-function printVolumeCone(height , radius){
-  let result = (1/3) * 3.14 * (radius**2) * height
-  console.log("Объем конуса равен " + result );
+function printVolumeCone(height, radius) {
+  let result = (1 / 3) * 3.14 * radius ** 2 * height;
+  console.log("Объем конуса равен " + result);
 }
 
-function printLengthStr (str){
+function printLengthStr(str) {
   console.log(" Длина строки равна  " + str.length);
 }
 
-function evenOrOdd (number){
-  if (number % 2 === 0 ){
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
     console.log(number + " число - четное");
-  }
-  else {
+  } else {
     console.log(number + " число - не четное");
   }
 }
 
-function getSquareNumber (number) {
+function getSquareNumber(number) {
   return "Квадрат этого числа равен  " + number ** 2;
 }
 
 function getRootOfANumber(number) {
-  return "Корень этого числа равен  " + number ** (1/2);
+  return "Корень этого числа равен  " + number ** (1 / 2);
 }
 
-function getPerimetrPentagon (a , b , c  , d , e) {
-  let result =  a + b + c + d + e;
-  return "Периметр пятиугольника равен " + result; 
+function getPerimetrPentagon(a, b, c, d, e) {
+  let result = a + b + c + d + e;
+  return "Периметр пятиугольника равен " + result;
 }
 
-for (let i = 0; i <= 33; i+=3) {
+for (let i = 0; i <= 33; i += 3) {
   console.log(i);
 }
 
 total = 1;
 for (let i = 1; i <= 8; i++) {
-  total = total * i
-}  
-console.log( "8! = " + total);
+  total = total * i;
+}
+console.log("8! = " + total);
 
 square = 1;
 for (let i = 1; i <= 10; i++) {
   square = i ** 2;
-  console.log( square);
-}  
+  console.log(square);
+}
 
 total = 1;
 for (let i = 1; i <= 25; i++) {
-  total = total + i
-}  
-console.log( total);
-
+  total = total + i;
+}
+console.log(total);
 
 for (let i = 0; i <= 20; i++) {
-  if (i % 2 === 0 ) {
+  if (i % 2 === 0) {
     console.log(i);
   }
 }
 
 for (let i = 1; i <= 15; i++) {
-  if (i % 2 ==! 0 ) {
+  if (i % 2 == !0) {
     console.log(i);
   }
 }
 
-
-let str = ["apple", "strawberry","pineapple"];
+let str = ["apple", "strawberry", "pineapple"];
 str.unshift("music", "ice");
-str.push("dog" , "car", "bird");
+str.push("dog", "car", "bird");
 
-let num = [2, 3, 4, 5 , 6, 8, 78];
+let num = [2, 3, 4, 5, 6, 8, 78];
 total = 0;
 for (let i = 0; i < num.length; i++) {
-  if (num[i] % 2 === 0){
+  if (num[i] % 2 === 0) {
     total += num[i];
   }
-}  
-console.log( total);
+}
+console.log(total);
 
-let string =  ["I", "like", "to", "eat!"];
-let sentence =  string.join(" ");
+let string = ["I", "like", "to", "eat!"];
+let sentence = string.join(" ");
 console.log(sentence);
 
 let numbers = ["one", "two", "three"];
-for (let i = numbers.length -1 ; i >= 0 ; i--) {
+for (let i = numbers.length - 1; i >= 0; i--) {
   console.log(numbers[i]);
 }
 
-let names = ["Aruuke", "Saltanat","Samat"];
-for (let i = 0; i < names.length; i++){
+let names = ["Aruuke", "Saltanat", "Samat"];
+for (let i = 0; i < names.length; i++) {
   console.log("Hello " + names[i]);
 }
 
-let num1 = [2 , 3, 4 ,5];
-for (let i = 0; i < num1.length; i++){
-  let result = num1[i]**2;
+let num1 = [2, 3, 4, 5];
+for (let i = 0; i < num1.length; i++) {
+  let result = num1[i] ** 2;
   console.log(result);
 }
+
+let presenceOfWords = ["cat", "dog", "snake"];
+function some (word){
+for (let i = 0; i <= presenceOfWords.length -1; i++) {
+  if (presenceOfWords.includes(word) === true) {
+    console.log(presenceOfWords[i] + " -true");
+  }
+  else {
+    console.log("false");
+  }
+}
+}
+
+
+
+
